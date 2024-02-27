69e6be7e3deb5eb23195b5b804ffed8965bf1183
Vishal21121 <vs84077000@gmail.com>
2024-02-27 16:27:19 +0530

    added: userRegisterValidator.ts

---
+++ server/src/validators/user.validator.ts
@@ -0,0 +1,27 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+    return [
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("Username should be provided")
+            .isLength({ min: 3 })
+            .withMessage("Username must of 3 characters"),
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Please enter a valid email address"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Please enter your password")
+            .isLength({ min: 8 })
+            .withMessage("Password must be at least 8 characters long"),
+
+    ]
+}
+
+export { userRegisterValidator }
