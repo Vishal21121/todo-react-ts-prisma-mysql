e1505256383d374847acb479e47f64c694374aad
Vishal21121 <vs84077000@gmail.com>
2024-02-27 23:35:58 +0530

    added: getTodo route

--- server/src/controllers/todo.controller.ts
+++ server/src/controllers/todo.controller.ts
@@ -43,3 +43,52 @@ export const createTodo = async (req: Request, res: Response) => {
         })
     }
 }
+
+export const getTodo = async (req: Request, res: Response) => {
+    const { userId } = req.params
+    if (!userId) {
+        return res.status(400).json({
+            success: false,
+            data: {
+                statusCode: 400,
+                message: "Please provide userId"
+            }
+        })
+    }
+    try {
+        const userFound = await prisma.user.findFirst({
+            where: {
+                id: userId
+            }
+        })
+        if (!userFound) {
+            return res.status(404).json({
+                success: false,
+                data: {
+                    statusCode: 404,
+                    message: "please provide correct userId"
+                }
+            })
+        }
+        const todos = await prisma.todo.findMany({
+            where: {
+                userId: userId
+            }
+        })
+        return res.status(200).json({
+            success: true,
+            data: {
+                statusCode: 200,
+                value: todos
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            data: {
+                statusCode: 500,
+                message: error || "Internal server error"
+            }
+        })
+    }
+}
--- server/src/routes/todo.routes.ts
+++ server/src/routes/todo.routes.ts
@@ -1,9 +1,10 @@
 import express from "express"
 import { createTodoValidator } from "../validators/todo.validator"
 import { validation } from "../middleware/validate.middleware"
-import { createTodo } from "../controllers/todo.controller"
+import { createTodo, getTodo } from "../controllers/todo.controller"
 const router = express.Router()
 
 router.route("/create").post(createTodoValidator(), validation, createTodo)
+router.route("/:userId").get(getTodo)
 
 export default router
