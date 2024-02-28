42d9b9d0c12ba02d3356032d313056641011048e
Vishal21121 <vs84077000@gmail.com>
2024-02-28 08:40:05 +0530

    added: update todo controller

--- server/src/controllers/todo.controller.ts
+++ server/src/controllers/todo.controller.ts
@@ -92,3 +92,71 @@ export const getTodo = async (req: Request, res: Response) => {
         })
     }
 }
+
+export const updateTodo = async (req: Request, res: Response) => {
+    const { content, isCompleted = false, todoId } = req.body
+    if (!content || typeof isCompleted != "boolean") {
+        return res.status(400).json({
+            success: false,
+            data: {
+                statusCode: 400,
+                message: "Please provide either content or isCompleted"
+            }
+        })
+    }
+    if (!todoId) {
+        return res.status(400).json({
+            success: false,
+            data: {
+                statusCode: 400,
+                message: "Please provide todoId"
+            }
+        })
+    }
+    try {
+        const todoFound = await prisma.todo.findFirst({
+            where: {
+                id: todoId
+            }
+        })
+        if (!todoFound) {
+            return res.status(404).json({
+                success: false,
+                data: {
+                    statusCode: 404,
+                    message: "please provide valid todoId"
+                }
+            })
+        }
+        const updatedTodo = await prisma.todo.update({
+            where: {
+                id: todoId
+            },
+            data: {
+                content: content,
+                isCompleted: isCompleted
+            }
+        })
+        console.log(updatedTodo);
+        return res.status(200).json(
+            {
+                success: true,
+                data: {
+                    statusCode: 200,
+                    message: "updated the todo",
+                    value: updatedTodo
+                }
+            }
+        )
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                data: {
+                    statusCode: 500,
+                    message: error || "Internal server error"
+                }
+            }
+        )
+    }
+}
--- server/src/routes/todo.routes.ts
+++ server/src/routes/todo.routes.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import { createTodoValidator } from "../validators/todo.validator"
 import { validation } from "../middleware/validate.middleware"
-import { createTodo, getTodo } from "../controllers/todo.controller"
+import { createTodo, getTodo, updateTodo } from "../controllers/todo.controller"
 const router = express.Router()
 
 router.route("/create").post(createTodoValidator(), validation, createTodo)
 router.route("/:userId").get(getTodo)
+router.route("/update").patch(updateTodo)
 
 export default router
