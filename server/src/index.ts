75105d26c121c1baa65e42cdbe440f5e8df322c9
Vishal21121 <vs84077000@gmail.com>
2024-02-28 08:57:18 +0530

    added: cors and made sourceMap: false in tsconfig

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import cors from "cors"
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes"
 import todoRouter from "./routes/todo.routes"
@@ -8,6 +9,7 @@ const app = express()
 
 
 app.use(express.json())
+app.use(cors())
 
 app.get("/heath", (req: Request, res: Response) => {
     res.status(200).send("up and running!")
