ad3ed9afaedece76339654086309cccea860757c
Vishal21121 <vs84077000@gmail.com>
2024-02-29 21:13:03 +0530

    configured tailwind and added Login.tsx

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,48 @@
+function Login() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-center lg:text-left">
+          <h1 className="text-4xl font-bold">Login now!</h1>
+        </div>
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <div className="flex gap-2 items-center mt-2">
+                <span className="text-xs w-fit">Don't have an account?</span>
+                <a href="#" className="label-text-alt link link-hover">
+                  Signup
+                </a>
+              </div>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
