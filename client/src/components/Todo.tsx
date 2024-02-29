5d0697e174c0da07edf78a6f3a8d40b6289819c3
Vishal21121 <vs84077000@gmail.com>
2024-02-29 23:20:24 +0530

    added: layout of Todo.tsx

--- client/src/components/Todo.tsx
+++ client/src/components/Todo.tsx
@@ -1,4 +1,21 @@
 function Todo() {
-  return <div>Todo</div>;
+  return (
+    <div className="w-full flex flex-col items-center">
+      <div className="w-3/4 sm:w-1/2 mt-8 flex flex-col flex-wrap items-center gap-4 p-4">
+        <h1 className="text-2xl font-bold">Create Todo</h1>
+        <div className="flex flex-col sm:flex-row sm:justify-center gap-4 w-full">
+          <input
+            type="text"
+            placeholder="What you have planned to do?"
+            className="input input-bordered w-full sm:w-3/4"
+          />
+          <button className="btn btn-neutral">Submit</button>
+        </div>
+      </div>
+      <div>
+        <h2 className="text-2xl font-bold">Saved Todos</h2>
+      </div>
+    </div>
+  );
 }
 export default Todo;
